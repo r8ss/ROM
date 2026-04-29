d1ac48342d02b120ed288d445b682cdd18af49a3
Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>
2026-04-29 03:18:27 +0100

    unica: mods: preload: update ksu apk
    Original author: @Android-Artisan
    Resources: https://github.com/ArtisanROM/ArtisanROM/commit/cc4935262681147d0d1df6a430122823417fe381

--- unica/mods/preload/customize.sh
+++ unica/mods/preload/customize.sh
@@ -1,4 +1,4 @@
-KERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.1.0/KernelSU_Next_v3.1.0-spoofed_33024-release.apk"
+ERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.2.0/KernelSU_Next_v3.2.0_33129-release.apk"
 # https://github.com/tiann/KernelSU/issues/886
 APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-mesa==/base.apk"
 
