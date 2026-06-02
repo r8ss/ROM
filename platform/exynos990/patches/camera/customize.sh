61d40d3b15453e6922e40e904e836934d0604d2a
redmax19 <alessandroaraujo0098@gmail.com>
2026-06-02 07:41:39 -0300

    Update customize.sh

--- platform/exynos990/patches/camera/customize.sh
+++ platform/exynos990/patches/camera/customize.sh
@@ -67,13 +67,11 @@ LOG_STEP_IN
 LOG "- Patching Video SVC Check"
 # Early jump after the log and abort functions when configureSVC fails
 # b LAB_001dd448 -> b LAB_001ddc24
-HEX_PATCH "$WORK_DIR/system/system/lib64/libstagefright.so" "da4a0594e0031a2a22feff17" "da4a0594e0031a2a19000014"
 
 
 LOG "- Patching HDR10+ Check"
 # Skip HDR10+ Recording ASSERT
 # cbz this,LAB_001dde38 -> nop
-HEX_PATCH "$WORK_DIR/system/system/lib64/libstagefright.so" "010140f90a4d0594604d0034" "010140f90a4d05941f2003d5"
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Adding prebuilt libs from other devices"
